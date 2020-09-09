497d9869d8e58b88bd7f3f396f8a8564bfcd0237
haithem711 <49453214+haithem711@users.noreply.github.com>
2020-09-09 08:34:22 +0100

    make it better

---
+++ config/dev.js
@@ -0,0 +1,4 @@
+module.exports={
+    MONGOURI:"mongodb+srv://haithem:99256188@movieapp.cq8fn.gcp.mongodb.net/Movies?retryWrites=true&w=majority",
+    
+}
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app=express()
 const mongoose = require('mongoose')
 const bodyParser=require('body-parser')
 const Movies=require('./models/movies')
-require('dotenv').config()
+const {MONGOURI}=require('./config/keys')
 const port=process.env.PORT || 5000
 var movie = require('./routes/movies');
 
 
 //create server mongoose
-mongoose.connect(process.env.DATABASE, {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true}, (err) => {
+mongoose.connect(MONGOURI, {useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true}, (err) => {
     if (err) throw err
     console.log('data base connected...')
 })
