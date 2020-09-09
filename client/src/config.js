a43c6d340b4f811e9c76ebf685f7f091d5d14c67
haithem711 <49453214+haithem711@users.noreply.github.com>
2020-09-09 13:44:51 +0100

    make it better

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV==='production'){
-   module.exports={APP_URL:"https://movietn.herokuapp.com/"} 
+   module.exports={APP_URL:'https://movietn.herokuapp.com'} 
 }else{
     module.exports = {APP_URL:'http://localhost:5000'}
 }
